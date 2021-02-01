8f3582ac93ed3a40445ef883b2208d4920302a76
Pedram Valiani <p.valiani@gmail.com>
2021-02-01 18:12:51 +0000

    tests for employees and managers working properly

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -1,2 +1,58 @@
+import Management.Manager;
+import Staff.Employee;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ManagerTest {
+
+
+    Manager manager;
+
+    //    create a new employee instance of Manager for testing
+    @Before
+    public void setUp(){
+
+        manager = new Manager("Pedram", 666, 50005, "Engineering");
+
+    }
+
+
+    @Test
+    public void getName(){
+        assertEquals("Pedram", manager.getName());
+
+    }
+
+    @Test
+    public void getNiNumber(){
+        assertEquals(666, manager.getNiNum());
+    }
+
+    @Test
+    public void getSalary(){
+        assertEquals(50005, manager.getSalary(), 0.01 );
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        manager.raiseSalary(1000);
+        assertEquals(51005, manager.getSalary(), 0.01);
+
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(500.05, manager.payBonus(), 0.01) ;
+
+    }
+
+    @Test
+    public void canGetDeptName(){
+        assertEquals("Engineering", manager.getDeptName());
+    }
+
+
+
 }
