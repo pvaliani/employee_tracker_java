11e92d05df13aec7e459c990831d93ae2d05d860
Pedram Valiani <p.valiani@gmail.com>
2021-02-01 17:27:44 +0000

    set up employee getters

--- src/main/java/Staff/Employee.java
+++ src/main/java/Staff/Employee.java
@@ -1,18 +1,32 @@
 package Staff;
 
 
-
 public abstract class Employee {
 
     private String name;
     private int niNum;
     private double salary;
 
+//  Employee constructor defines name, national insurance number and salary
     public Employee(String name, int niNum, double salary){
         this.name = name;
         this.niNum = niNum;
         this.salary = salary;
 
+    }
+
+//    getter methods for constructor
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNiNum() {
+        return niNum;
+    }
+
+    public double getSalary() {
+        return salary;
     }
 
 }
