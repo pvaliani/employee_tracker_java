0d3927648ad86ec9e702d4b7cd6ea72879593525
Pedram Valiani <p.valiani@gmail.com>
2021-02-01 17:46:33 +0000

    adds manager subclass of employee

--- src/main/java/Management/Manager.java
+++ src/main/java/Management/Manager.java
@@ -1,4 +1,20 @@
 package Management;
 
-public class Manager {
+import Staff.Employee;
+
+// Manager constructor initialised with deptName as a new parameter set up
+
+public class Manager extends Employee {
+
+    private String deptName;
+
+    public Manager(String name, int niNum, double salary, String deptName){
+        super(name, niNum, salary);
+        this.deptName = deptName;
+    }
+//    getter method for department name
+    public String getDeptName(){
+        return this.deptName;
+    }
+
 }
