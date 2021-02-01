234e9cd6bf23eff76b11c0e24b13b667650a87fb
Pedram Valiani <p.valiani@gmail.com>
2021-02-01 17:39:36 +0000

    adds comments

--- src/main/java/techStaff/Developer.java
+++ src/main/java/techStaff/Developer.java
@@ -2,6 +2,8 @@ package techStaff;
 
 import Staff.Employee;
 
+// Developer is a subclass of the abstract Employee superclass
+
 public class Developer extends Employee {
 
     public Developer(String name, int niNum, double salary){
