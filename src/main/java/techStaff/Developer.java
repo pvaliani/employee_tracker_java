aa2a14389d10a7f5f25f45ab5ab9fd8f5aea7d80
Pedram Valiani <p.valiani@gmail.com>
2021-02-01 17:38:44 +0000

    adds developer subclass

--- src/main/java/techStaff/Developer.java
+++ src/main/java/techStaff/Developer.java
@@ -1,4 +1,11 @@
 package techStaff;
 
-public class Developer {
+import Staff.Employee;
+
+public class Developer extends Employee {
+
+    public Developer(String name, int niNum, double salary){
+        super(name, niNum, salary);
+
+    }
 }
