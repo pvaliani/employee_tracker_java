bde37d1dedc7d800d36afc22e167e5598424cf08
Pedram Valiani <p.valiani@gmail.com>
2021-02-01 17:26:29 +0000

    added employee info

--- src/main/java/Staff/Employee.java
+++ src/main/java/Staff/Employee.java
@@ -1,4 +1,18 @@
 package Staff;
 
-public class Employee {
+
+
+public abstract class Employee {
+
+    private String name;
+    private int niNum;
+    private double salary;
+
+    public Employee(String name, int niNum, double salary){
+        this.name = name;
+        this.niNum = niNum;
+        this.salary = salary;
+
+    }
+
 }
