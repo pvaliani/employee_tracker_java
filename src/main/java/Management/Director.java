3433fcb721fc4f16ae683e3c659e1d2534017e0b
Pedram Valiani <p.valiani@gmail.com>
2021-02-01 18:29:30 +0000

    updates director class constructor

--- src/main/java/Management/Director.java
+++ src/main/java/Management/Director.java
@@ -1,4 +1,15 @@
 package Management;
 
-public class Director {
+import Staff.Employee;
+
+public class Director extends Employee {
+
+
+    private Double budget;
+
+    public Director(String name, int niNum, double salary, double budget){
+        super(name, niNum, salary);
+        this.budget = budget;
+
+    }
 }
