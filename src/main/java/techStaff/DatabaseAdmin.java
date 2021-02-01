fe8111e3f5bfde04ba854bbf7b5b90e324036a71
Pedram Valiani <p.valiani@gmail.com>
2021-02-01 17:41:46 +0000

    adds DatabaseAdmin subclass

--- src/main/java/techStaff/DatabaseAdmin.java
+++ src/main/java/techStaff/DatabaseAdmin.java
@@ -1,4 +1,11 @@
 package techStaff;
 
-public class DatabaseAdmin {
+import Staff.Employee;
+
+public class DatabaseAdmin extends Employee {
+
+    public DatabaseAdmin(String name, int niNum, double salary){
+        super(name, niNum, salary);
+    }
+
 }
